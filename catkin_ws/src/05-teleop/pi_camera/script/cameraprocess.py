14119e80d09bffa443f2f6878e585c2c158044e6
himarsmty <1584666784@qq.com>
2018-12-14 09:16:40 +0800

    add camera

--- catkin_ws/src/05-teleop/pi_camera/script/cameraprocess.py
+++ catkin_ws/src/05-teleop/pi_camera/script/cameraprocess.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python
+# coding: utf-8
+import rospy
+from sensor_msgs.msg import Image
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+import sys
+
+
+def SciImagePub():
+    rospy.init_node('cameraprocess', anonymous=True)
+    img_pub = rospy.Publisher('/sci/image_raw', Image, queue_size=2)
+    rate = rospy.Rate(5)
+    cap = cv2.VideoCapture(0)
+    scaling_factor = 0.5
+    bridge = CvBridge()
+    if not cap.isOpened():
+        sys.stdout.write('sci camera is not open!')
+        return -1
+    count = 0
+    while not rospy.is_shutdown():
+        ret, frame = cap.read()
+        if ret:
+            count += 1
+        else:
+            rospy.loginfo('capturing image failed.')
+        if count == 2:
+            count = 0
+            frame = cv2.resize(
+                frame,
+                None,
+                fx=scaling_factor,
+                fy=scaling_factor,
+                imterpolation=cv2.INTER_AREA)
+            msg = bridge.cv2_to_imgmsg(frame, encoding='bgr8')
+            print ('publishing frame ...')
+        rate.sleep()
+    
+    if __name__ == '__main__':
+        try:
+            SciImagePub()
+        except rospy.ROSInternalException:
+            pass 
+
