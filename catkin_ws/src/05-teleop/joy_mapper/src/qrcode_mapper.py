7eb55de470a12a6504c4c786660efc7f59ad6722
himarsmty <1584666784@qq.com>
2018-12-15 01:11:18 +0800

    s

--- catkin_ws/src/05-teleop/joy_mapper/src/qrcode_mapper.py
+++ catkin_ws/src/05-teleop/joy_mapper/src/qrcode_mapper.py
@@ -13,16 +13,14 @@ def callback(msg):
     car_cmd_msg.header.seq = 0
     now = rospy.get_rostime()
     car_cmd_msg.header.stamp = now
+    print(msg.data)
     if msg.data == '前进':
-        print(msg.data)
         car_cmd_msg.v = 0.1
         car_cmd_msg.omega = 0.0
     if msg.data == '右转':
-        print(msg.data)
         car_cmd_msg.v = 0.0
         car_cmd_msg.omega = -1.0
     if msg.data == '左转':
-        print(msg.data)
         car_cmd_msg.v = 0.0
         car_cmd_msg.omega = 1.0
     pub_car_cmd.publish(car_cmd_msg)
